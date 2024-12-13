b385cfbe930e8cc7f86f000b2080e29aaf8fafac
주재범 <jaebum7396@naver.com>
2024-12-13 16:43:57 +0900

    feat. sample page 매핑

--- src/main/java/jcore/view/ViewController.java
+++ src/main/java/jcore/view/ViewController.java
@@ -7,16 +7,39 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ViewController {
+    @GetMapping("/admin/charts")
+    public String charts() {
+        return "admin/charts";
+    }
     @RequestMapping("/admin/index")
     public ModelAndView adminIndex() throws Exception {
-        System.out.println("admin index");
         ModelAndView mv = new ModelAndView();
         mv.setViewName("admin/index");
         return mv;
     }
+    @GetMapping("/admin/layout-sidenav-light")
+    public String layoutSidenavLight() {
+        return "admin/layout-sidenav-light";
+    }
+    @GetMapping("/admin/layout-static")
+    public String layoutstatic() {
+        return "admin/layout-static";
+    }
     @GetMapping("/admin/login")
     public String adminUser() {
         return "admin/login";
+    }
+    @GetMapping("/admin/password")
+    public String password() {
+        return "admin/password";
+    }
+    @GetMapping("/admin/register")
+    public String register() {
+        return "admin/register";
+    }
+    @GetMapping("/admin/tables")
+    public String tables() {
+        return "admin/tables";
     }
     @GetMapping("/policy/privacy_policy")
     public String privacyPolicy() {
