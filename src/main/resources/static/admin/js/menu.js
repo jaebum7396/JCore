8d5a1becff7c8205e9cf3794c25f9f73709fe008
주재범 <jaebum7396@naver.com>
2024-12-18 17:28:53 +0900

    feat. menu 동적으로 동작하도록

--- src/main/resources/static/admin/js/menu.js
+++ src/main/resources/static/admin/js/menu.js
@@ -161,7 +161,6 @@ class SideMenuGenerator extends BaseMenuGenerator {
 		return `
             <div class="sb-sidenav-footer">
                 <div class="small">Logged in as:</div>
-                Start Bootstrap
             </div>
         `;
 	}
